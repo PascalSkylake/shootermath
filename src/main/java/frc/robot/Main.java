7304d177b30864e06b97eb37c07d048edd300a63
PascalSkylake <jackcammarata1@gmail.com>
2024-02-10 03:02:49 -0500

    first commit

---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.numbers.N4;
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+  private Main() {
+  }
+
+  static Shooter shooter = new Shooter();
+  static double height = 0.5;
+  static double v0 = 20;
+
+  public static void main(String... args) {
+    calcAllIntersects();
+  }
+
+  public static void calcAllIntersects() {
+    // Vector<N4> in = VecBuilder.fill(0, 0.5, 20 * Math.cos(Math.PI/6), 20 *
+    // Math.sin(Math.PI/6));
+    // shooter.propagateState(in, 4, 100);
+
+    for (double theta = 0; theta < Math.PI / 2; theta += 0.002) {
+      Vector<N4> in = VecBuilder.fill(0, 0.5, 20 * Math.cos(theta), 20 * Math.sin(theta));
+      Optional<Double> a = shooter.calcTrajectoryIntersectWithSpeakerHeightPlane(in, 0.0001);
+
+      if (a.isPresent()) {
+        //System.out.println("" + theta + "," + a.get());
+      }
+    }
+  }
+
+}
---
+++ src/main/java/frc/robot/Shooter.java
@@ -0,0 +1,85 @@
+package frc.robot;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.numbers.N4;
+import edu.wpi.first.math.numbers.N5;
+
+
+
+public class Shooter {
+  Function<Vector<N5>, Vector<N5>> projectileEquation;
+
+  private final double SPEAKER_HEIGHT = 3;
+  private final double DRAG_COEFFICIENT = 0.5;
+  private final double AIR_DENSITY = 1.225;
+  private final double CROSS_SECTIONAL_AREA = 0.018;
+  private final double NOTE_MASS = 0.2353;
+  private final double MU = (DRAG_COEFFICIENT * AIR_DENSITY * CROSS_SECTIONAL_AREA) / (2 * NOTE_MASS);
+
+  public Shooter() {
+
+
+    projectileEquation = (Vector<N5> x) -> {
+      double[] in = x.getData();
+      double vx = in[2];
+      double vy = in[3];
+      double v = Math.sqrt((vx * vx) + (vy * vy));
+      double ax = -MU * vx * v;
+      double ay = -9.8 - (MU * vy * v);
+
+      Vector<N5> out = VecBuilder.fill(vx, vy, ax, ay, 0);
+
+      return out;
+    };
+  }
+
+  public Vector<N5> rkFour(Vector<N5> x) {
+    double h = x.get(4, 0);
+
+    Vector<N5> k_1 = projectileEquation.apply(x);
+    Vector<N5> k_2 = projectileEquation.apply(x.plus(k_1.times(h / 2.0)));
+    Vector<N5> k_3 = projectileEquation.apply(x.plus(k_2.times(h / 2.0)));
+    Vector<N5> k_4 = projectileEquation.apply(x.plus(k_3.times(h)));
+  
+    Vector<N5> out = x.plus((k_1.plus(k_2.times(2)).plus(k_3.times(2)).plus(k_4)).times(h / 6.0));
+    return out; 
+  }
+
+  public Vector<N5> propagateState(Vector<N4> x, double t, int intervals) {
+    double dt = t / intervals;
+    Vector<N5> state = VecBuilder.fill(x.get(0, 0), x.get(1, 0), x.get(2, 0), x.get(3, 0), dt);
+    for (int i = 0; i < intervals; i++) {
+      state = rkFour(state);
+      //System.out.println("" + (i * dt) + "," + state.get(0, 0) + "," + state.get(1,0) + "," + state.get(2, 0) + "," + state.get(3, 0));
+      
+    }
+
+    return state;
+  }
+
+  public Optional<Double> calcTrajectoryIntersectWithSpeakerHeightPlane(Vector<N4> x, double dt) {
+    Vector<N5> state = VecBuilder.fill(x.get(0, 0), x.get(1, 0), x.get(2, 0), x.get(3, 0), dt);
+    double counter = 0;
+
+    while (state.get(3, 0) > 0) {
+
+      counter += dt;
+      state = rkFour(state);
+
+
+      if (state.get(1, 0) > SPEAKER_HEIGHT) {
+        double timeAgo = (state.get(1, 0) - SPEAKER_HEIGHT) / state.get(3, 0);
+        double intersectX = state.get(0, 0) - (timeAgo * state.get(2, 0));
+        System.out.println(counter + "," + intersectX);
+        return Optional.of(intersectX);
+      }
+      
+    }
+
+    return Optional.empty();
+  }
+}
