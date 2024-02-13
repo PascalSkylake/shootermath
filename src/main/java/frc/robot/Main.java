57cf23a2b23e183a83aaf8eb95b141353d57dc9e
PascalSkylake <jackcammarata1@gmail.com>
2024-02-13 01:28:01 -0500

    tons of stuff, needs updating to arrays

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -4,13 +4,30 @@
 
 package frc.robot;
 
+import java.util.ArrayList;
 import java.util.Optional;
+
+import org.apache.commons.math3.analysis.MultivariateFunction;
+import org.apache.commons.math3.analysis.MultivariateVectorFunction;
+import org.apache.commons.math3.fitting.leastsquares.MultivariateJacobianFunction;
+import org.apache.commons.math3.optim.InitialGuess;
+import org.apache.commons.math3.optim.MaxEval;
+import org.apache.commons.math3.optim.MaxIter;
+import org.apache.commons.math3.optim.OptimizationData;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+import org.apache.commons.math3.optim.nonlinear.scalar.MultivariateOptimizer;
+import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction;
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.PowellOptimizer;
+import org.apache.commons.math3.optim.nonlinear.vector.MultivariateVectorOptimizer;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N4;
+import edu.wpi.first.math.numbers.N6;
+import edu.wpi.first.math.numbers.N8;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
@@ -20,32 +37,85 @@ public final class Main {
   static Shooter shooter = new Shooter();
   static double height = 0.5;
   static double v0 = 20;
+  static double x = 0;
+  static double y = 0;
+  static double z = 0;
+  static double vx = 0;
+  static double vy = 0;
 
   public static void main(String... args) {
-    calcAllIntersects();
-  }
+    try {
+    Thread.sleep(500);
+    } catch (Exception exception) {
 
-  public static void calcAllIntersects() {
-    // Vector<N4> in = VecBuilder.fill(0, 0.5, 20 * Math.cos(Math.PI/6), 20 *
-    // Math.sin(Math.PI/6));
-    // shooter.propagateState(in, 4, 100);
-    // double theta = 1;
-
-    // Translation2d shooterExit = shooter.shooterExitRobotRelative(theta);
-    // Vector<N4> in = VecBuilder.fill(shooterExit.getX(), shooterExit.getY(), 20 * Math.cos(theta), 20 * Math.sin(theta));
-    // shooter.propagateState(in, 3, 50);
-
-    for (double theta = 0; theta < Math.PI / 2; theta += 0.002) {
-      Translation2d shooterExit = shooter.shooterExitRobotRelative(theta);
-      Vector<N4> in = VecBuilder.fill(shooterExit.getX(), shooterExit.getY(), 20 * Math.cos(theta), 20 * Math.sin(theta));
-      Optional<Double> a = shooter.calcTrajectoryIntersectWithSpeakerHeightPlane(in, 0.0001);
-
-      if (a.isPresent()) {
-        //System.out.println("" + theta + "," + a.get());
-      }
     }
+    long start = System.nanoTime();
+    //double[] optimal = optimizeShooterOrientation(0.25, Math.PI / 2, 0.05, 0, 3, 2);
+    //System.out.println(optimal[0] + "," + optimal[1] + "," + optimal[2]);
+
+    //Translation2d shooterExit = shooter.shooterExitRobotRelative(optimal[0]);
+    // Vector<N6> in = VecBuilder.fill(Main.x, Main.y, Main.z,
+    //         Main.vx + (Main.v0 * Math.sin(Math.PI / 2 - optimal[0]) * Math.cos(optimal[1])),
+    //         Main.vy + (Main.v0 * Math.sin(Math.PI / 2 - optimal[0]) * Math.sin(optimal[1])), Main.v0 * Math.sin(Math.PI / 2 - optimal[0]));
+    // ArrayList<Vector<N8>> trajectory = shooter.propagateWholeTrajectory3d(in, optimal[2], 100);
+
+    Vector<N6> in = VecBuilder.fill(Main.x, Main.y, Main.z,
+            Main.vx + (Main.v0 * Math.sin(Math.PI / 2 - 1) * Math.cos(1)),
+            Main.vy + (Main.v0 * Math.sin(Math.PI / 2 - 1) * Math.sin(1)), Main.v0 * Math.sin(Math.PI / 2 - 1));
+        //ArrayList<Vector<N8>> trajectory = shooter.propagateWholeTrajectory3d(in, 1, 20);
+        shooter.propagateWholeTrajectory3d(in, 1, 20);
+    long end = System.nanoTime();
+
+    System.out.println((end - start) / 1000000.0);
+
+    //Vector<N8> v = trajectory.get(trajectory.size() - 1);
+
+    // for (Vector<N8> v : trajectory) {
+    //   System.out.println(v.get(0, 0) + "," + v.get(1,0) + "," + v.get(2, 0) + "," + v.get(5, 0) + "," + v.get(6, 0));
+    // }
+    //System.out.println(v.get(0, 0) + "," + v.get(1,0) + "," + v.get(2, 0) + "," + v.get(5, 0) + "," + v.get(6, 0));
   }
 
 
+
+  private static double[] optimizeShooterOrientation(double initialTheta, double initialPhi, double initialTime, double targetX, double targetY, double targetZ) {
+
+    MultivariateFunction f = new MultivariateFunction() {
+      @Override
+      public double value(double[] point) {
+        // calculate trajectory given theta phi and t
+
+        Vector<N6> in = VecBuilder.fill(Main.x, Main.y, Main.z,
+            Main.vx + (Main.v0 * Math.sin(Math.PI / 2 - point[0]) * Math.cos(point[1])),
+            Main.vy + (Main.v0 * Math.sin(Math.PI / 2 - point[0]) * Math.sin(point[1])), Main.v0 * Math.sin(Math.PI / 2 - point[0]));
+        ArrayList<Vector<N8>> trajectory = shooter.propagateWholeTrajectory3d(in, point[2], 20);
+        Vector<N8> finalPosition = trajectory.get(trajectory.size() - 1);
+
+        Translation3d target = new Translation3d(targetX, targetY, targetZ);
+        Translation3d finalPose = new Translation3d(finalPosition.get(0, 0), finalPosition.get(1, 0), finalPosition.get(2, 0));
+
+
+        //System.out.println(finalPose);
+        return target.getDistance(finalPose);
+      }
+
+    };
+
+    ObjectiveFunction objective = new ObjectiveFunction(f);
+
+
+
+    PowellOptimizer optimizer = new PowellOptimizer(0.000001, 0.000001);
+    double[] initialGuess = new double[] { initialTheta, initialPhi, initialTime };
+    InitialGuess guess = new InitialGuess(initialGuess);
+    MaxIter maxIter = new MaxIter(500);
+    // look at my lawyer dawg I'm goin to jail!!!
+    MaxEval maxEval = new MaxEval(500);
+    GoalType goalType = GoalType.MINIMIZE;
+
+    double[] optimalParams = optimizer.optimize(new OptimizationData[] {objective, guess, maxIter, maxEval, goalType}).getPoint();
+
+    return optimalParams;
+  }
 
 }
--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -1,18 +1,20 @@
 package frc.robot;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.function.Function;
 
+import edu.wpi.first.math.Num;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N4;
-import edu.wpi.first.math.numbers.N5;
-
-
+import edu.wpi.first.math.numbers.N6;
+import edu.wpi.first.math.numbers.N8;
 
 public class Shooter {
-  Function<Vector<N5>, Vector<N5>> projectileEquation;
+  Function<Vector<N6>, Vector<N6>> projectileEquation;
+  Function<Vector<N8>, Vector<N8>> projectileEquation3d;
 
   private final double SPEAKER_HEIGHT = 1.9812;
   private final double DRAG_COEFFICIENT = 0.5;
@@ -24,68 +26,131 @@ public class Shooter {
   private final double SHOOTER_PIVOT_TO_END = 0.37516;
   private final Translation2d SHOOTER_PIVOT_ROBOT_REL = new Translation2d(-0.2757, 0.5972);
 
-
   public Shooter() {
 
-
-    projectileEquation = (Vector<N5> x) -> {
-      double[] in = x.getData();
-      double vx = in[2];
-      double vy = in[3];
+    projectileEquation = (Vector<N6> x) -> {
+      double vx = x.get(2, 0);
+      double vy = x.get(3, 0);
       double v = Math.sqrt((vx * vx) + (vy * vy));
       double ax = -MU * vx * v;
       double ay = -9.8 - (MU * vy * v);
 
-      Vector<N5> out = VecBuilder.fill(vx, vy, ax, ay, 0);
+      Vector<N6> out = VecBuilder.fill(vx, vy, ax, ay, 0, 0);
+
+      return out;
+    };
+
+    projectileEquation3d = (Vector<N8> x) -> {
+      double vx = x.get(3, 0);
+      double vy = x.get(4, 0);
+      double vz = x.get(5, 0);
+      double v = Math.sqrt((vx * vx) + (vy * vy) + (vz * vz));
+      double ax = -MU * vx * v;
+      double ay = -MU * vy * v;
+      double az = -9.8 - (MU * vz * v);
+
+      Vector<N8> out = VecBuilder.fill(vx, vy, vz, ax, ay, az, 0, 0);
 
       return out;
     };
   }
 
-  public Vector<N5> rkFour(Vector<N5> x) {
-    double h = x.get(4, 0);
+  public <R extends Num> Vector<R> rkFour(Vector<R> x, Function<Vector<R>, Vector<R>> f) {
+    double h = x.get(x.getNumRows() - 1, 0);
 
-    Vector<N5> k_1 = projectileEquation.apply(x);
-    Vector<N5> k_2 = projectileEquation.apply(x.plus(k_1.times(h / 2.0)));
-    Vector<N5> k_3 = projectileEquation.apply(x.plus(k_2.times(h / 2.0)));
-    Vector<N5> k_4 = projectileEquation.apply(x.plus(k_3.times(h)));
-  
-    Vector<N5> out = x.plus((k_1.plus(k_2.times(2)).plus(k_3.times(2)).plus(k_4)).times(h / 6.0));
-    return out; 
+    Vector<R> k_1 = f.apply(x);
+    Vector<R> k_2 = f.apply(x.plus(k_1.times(h / 2.0)));
+    Vector<R> k_3 = f.apply(x.plus(k_2.times(h / 2.0)));
+    Vector<R> k_4 = f.apply(x.plus(k_3.times(h)));
+
+    Vector<R> out = x.plus((k_1.plus(k_2.times(2)).plus(k_3.times(2)).plus(k_4)).times(h / 6.0));
+    out.set(x.getNumRows() - 2, 0, x.get(x.getNumRows() - 2, 0) + h);
+    return out;
   }
 
-  public Vector<N5> propagateState(Vector<N4> x, double t, int intervals) {
+  public Vector<N6> propagateState(Vector<N4> x, double t, int intervals) {
     double dt = t / intervals;
-    Vector<N5> state = VecBuilder.fill(x.get(0, 0), x.get(1, 0), x.get(2, 0), x.get(3, 0), dt);
+    Vector<N6> state = VecBuilder.fill(x.get(0, 0), x.get(1, 0), x.get(2, 0), x.get(3, 0), dt, 0);
     for (int i = 0; i < intervals; i++) {
-      state = rkFour(state);
-      System.out.println("" + (i * dt) + "," + state.get(0, 0) + "," + state.get(1,0) + "," + state.get(2, 0) + "," + state.get(3, 0));
-      
+      state = rkFour(state, projectileEquation);
+      System.out.println("" + (i * dt) + "," + state.get(0, 0) + "," + state.get(1, 0) + "," + state.get(2, 0) + ","
+          + state.get(3, 0));
+
     }
 
     return state;
   }
 
-  public Optional<Double> calcTrajectoryIntersectWithSpeakerHeightPlane(Vector<N4> x, double dt) {
-    Vector<N5> state = VecBuilder.fill(x.get(0, 0), x.get(1, 0), x.get(2, 0), x.get(3, 0), dt);
-    double counter = 0;
+  public Optional<Double> calcTrajectoryIntersectWithSpeakerHeightPlane(Vector<N4> k, double dt) {
+    double x = k.get(0, 0);
+    double y = k.get(1, 0);
+    double vx = k.get(3, 0);
+    double vy = k.get(4, 0);
+    Vector<N6> state = VecBuilder.fill(x, y, vx, vy, 0, dt);
 
     while (state.get(3, 0) > 0) {
 
-      counter += dt;
-      state = rkFour(state);
-
+      state = rkFour(state, projectileEquation);
 
       if (state.get(1, 0) > SPEAKER_HEIGHT) {
         double timeAgo = (state.get(1, 0) - SPEAKER_HEIGHT) / state.get(3, 0);
         double intersectX = state.get(0, 0) - (timeAgo * state.get(2, 0));
-        System.out.println(counter + "," + intersectX);
+        //System.out.println(state.get(4, 0) + "," + intersectX);
         return Optional.of(intersectX);
       }
-      
+
     }
 
     return Optional.empty();
+  }
+
+  public Optional<Double> calcTrajectoryIntersectWithSpeakerHeightPlane3d(Vector<N6> k, double dt) {
+    double x = k.get(0, 0);
+    double y = k.get(1, 0);
+    double z = k.get(2, 0);
+    double vx = k.get(3, 0);
+    double vy = k.get(4, 0);
+    double vz = k.get(5, 0);
+
+    Vector<N8> state = VecBuilder.fill(x, y, z, vx, vy, vz, 0, dt);
+
+    while (state.get(5, 0) > 0) {
+
+      state = rkFour(state, projectileEquation3d);
+      // System.out.println(state.get(2, 0) + "," + state.get(5, 0));
+
+      if (state.get(2, 0) > SPEAKER_HEIGHT) {
+        double timeAgo = (state.get(2, 0) - SPEAKER_HEIGHT) / state.get(5, 0);
+        double intersectX = state.get(0, 0) - (timeAgo * state.get(3, 0));
+        double intersectY = state.get(1, 0) - (timeAgo * state.get(4, 0));
+        System.out.println(state.get(6, 0) + "," + intersectX + "," + intersectY);
+        return Optional.of(intersectX);
+      }
+
+    }
+
+    return Optional.empty();
+  }
+
+  public ArrayList<Vector<N8>> propagateWholeTrajectory3d(Vector<N6> k, double t, int intervals) {
+    double x = k.get(0, 0);
+    double y = k.get(1, 0);
+    double z = k.get(2, 0);
+    double vx = k.get(3, 0);
+    double vy = k.get(4, 0);
+    double vz = k.get(5, 0);
+
+    ArrayList<Vector<N8>> out = new ArrayList<>(intervals);
+    double dt = t / intervals;
+
+    Vector<N8> state = VecBuilder.fill(x, y, z, vx, vy, vz, 0, dt);
+
+    for (int i = 0; i < intervals; i++) {
+      state = rkFour(state, projectileEquation3d);
+      out.add(state);
+    }
+    
+    return out;
   }
 
   public Translation2d shooterExitRobotRelative(double theta) {
@@ -95,5 +160,4 @@ public class Shooter {
     return SHOOTER_PIVOT_ROBOT_REL.plus(new Translation2d(x, y));
   }
 
-  
 }
